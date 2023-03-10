64679fcfd4a01ffbb38bfdbd05596a7aa47fc0dd
apatel924 <apatel2@ualberta.ca>
2023-03-10 10:13:10 +0000

    update postgresql for parameterized quries

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
+const queryString = `
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers 
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+JOIN cohorts ON cohort_id = cohorts.id WHERE cohorts.name LIKE $1
+GROUP BY cohort, teacher
+ORDER BY teacher`;
+
+const values = [`%${process.argv[2]}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
+.catch(err => console.error('query error', err.stack));
